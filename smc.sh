b8c455dfd96fcf875ace2f5660137c7c06e39b08
hwipl <33433250+hwipl@users.noreply.github.com>
2019-10-19 22:11:22 +0200

    add smc.sh
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ smc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+CMD=$1
+
+# usage
+USAGE="Usage:
+    $0 check"
+
+# check if smc support is in the kernel
+function check_kernel {
+	# check /proc/config.gz
+	echo -n "Kernel support for SMC: "
+	if zgrep "^CONFIG_SMC=y" /proc/config.gz > /dev/null; then
+		echo "yes"
+	elif zgrep "^CONFIG_SMC=m" /proc/config.gz > /dev/null; then
+		echo "module"
+	else
+		echo "no"
+	fi
+}
+
+# run commands with other command line arguments
+case "$CMD" in
+	"check")
+		check_kernel
+		;;
+	*)
+		echo "$USAGE"
+		;;
+esac
