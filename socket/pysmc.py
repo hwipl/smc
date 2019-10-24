5f58f670cd89aff53b5d49cddcd790625ed8d599
hwipl <33433250+hwipl@users.noreply.github.com>
2019-10-24 02:02:21 +0200

    socket/pysmc.py: use bind(), accept(), connect() from libc
    Use libc socket functions bind(), accept(), and connect() with ctypes
    instead of socket module functions because they do not work with AF_SMC.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- socket/pysmc.py
+++ socket/pysmc.py
@@ -6,6 +6,10 @@ Based on echo client and server examples in official python documentation
 (https://docs.python.org/3/library/socket.html#example)
 """
 
+import ctypes.util
+import ctypes
+
+import ipaddress
 import argparse
 import socket
 import sys
@@ -19,6 +23,68 @@ SMCPROTO_SMC6 = 1
 # client/server definitions
 PORT = 50000
 
+# libc
+LIBC_PATH = ctypes.util.find_library("c")
+LIBC = ctypes.CDLL(LIBC_PATH)
+
+
+class SockaddrIn(ctypes.Structure):
+    """
+    Socket Address for IPv4 struct for ctypes
+    """
+    _fields_ = [("sin_family", ctypes.c_ushort),
+                ("sin_port", ctypes.c_uint16),
+                ("sin_addr", ctypes.c_uint32),
+                ("sin_zero", ctypes.c_ubyte * 8)]
+
+    def __init__(self, address=0, port=0):
+        super(SockaddrIn, self).__init__()
+        self.sin_family = ctypes.c_ushort(socket.AF_INET)
+        self.sin_port = ctypes.c_uint16(socket.htons(int(port)))
+        self.sin_addr = ctypes.c_uint32(socket.htonl(int(
+            ipaddress.IPv4Address(address))))
+
+    def __len__(self):
+        return ctypes.sizeof(self)
+
+    def __repr__(self):
+        addr = ipaddress.IPv4Address(socket.ntohl(self.sin_addr))
+        port = socket.ntohs(self.sin_port)
+        return f"{addr}:{port}"
+
+
+def bind(sock, address, port):
+    """
+    bind() using libc with ctypes
+    """
+
+    # bind address and port
+    sockaddr = SockaddrIn(address, port)
+    LIBC.bind(sock.fileno(), ctypes.byref(sockaddr), len(sockaddr))
+
+
+def accept(sock):
+    """
+    accept() using libc with ctypes
+    """
+
+    # accept connection
+    sockaddr = SockaddrIn()
+    sockaddr_len = ctypes.c_int(len(sockaddr))
+    client_sock = LIBC.accept(sock.fileno(), ctypes.byref(sockaddr),
+                              ctypes.byref(sockaddr_len))
+    return socket.socket(fileno=client_sock), sockaddr
+
+
+def connect(sock, address, port):
+    """
+    connect() using libc with ctypes
+    """
+
+    # connect to server
+    sockaddr = SockaddrIn(address, port)
+    LIBC.connect(sock.fileno(), ctypes.byref(sockaddr), len(sockaddr))
+
 
 def check():
     """
@@ -30,7 +96,8 @@ def check():
     print("Checking SMC socket")
     sock = socket.socket(AF_SMC, socket.SOCK_STREAM, SMCPROTO_SMC)
     sock.fileno()
-    sock.bind(("", PORT))
+    # sock.bind() does not work with SMC, use libc version
+    bind(sock, "0.0.0.0", PORT)
     sock.close()
 
 
@@ -42,18 +109,20 @@ def server(host, port):
 
     # set host address and port
     if not host:
-        host = ""
+        host = "0.0.0.0"
     if not port:
         port = PORT
 
     # start server
     print("Starting SMC server")
     with socket.socket(AF_SMC, socket.SOCK_STREAM, SMCPROTO_SMC) as sock:
-        sock.bind((host, port))
+        # sock.bind() does not work with SMC, use libc version
+        bind(sock, host, port)
         sock.listen(1)
-        conn, addr = sock.accept()
+        # sock.accept() does not work with SMC, use libc version
+        conn, addr = accept(sock)
         with conn:
-            print("Connected: ", addr)
+            print("Connected:", addr)
             while True:
                 data = conn.recv(1024)
                 if not data:
@@ -69,14 +138,15 @@ def client(host, port):
 
     # set host address and port
     if not host:
-        host = "localhost"
+        host = "127.0.0.1"
     if not port:
         port = PORT
 
     # start client
     print("Starting SMC client")
     with socket.socket(AF_SMC, socket.SOCK_STREAM, SMCPROTO_SMC) as sock:
-        sock.connect((host, port))
+        # sock.connect() does not work with SMC, use libc version
+        connect(sock, host, port)
         sock.sendall(b"Hello, world")
         data = sock.recv(1024)
         print('Received:', repr(data))
